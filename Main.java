13f8d35af27e7805a38c868affed428b4b4e630b
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-01-29 21:53:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+//Create a program to print Bismillah Hir Rahman Nir Rahim
+
+public class Main {
+    public static void main(String[] agrs) {
+        System.out.println("Bismillah Hir Rahman Nir Rahim");
+    }
+}
