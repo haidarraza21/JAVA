563f9c02500d535f66adbd6fbf4ae585082bb775
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:02:50 +0530

    Update GradeBasedOnMarks.java

---
+++ GradeBasedOnMarks.java
@@ -0,0 +1,27 @@
+/*
+Create a program that calculates grades based on marks
+A-> above 90%        B-> above 75%
+C-> above 60%        D-> above 30%
+F-. below 30%*/
+import java.util.Scanner;
+public class GradeBasedOnMarks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Please enter your marks :");
+        int marks = input.nextInt();
+
+        if(marks > 90){
+            System.out.println("Grade A");
+        } else if(marks>75){
+            System.out.println("Grade B");
+        } else if(marks > 60){
+            System.out.println("Grade C");
+        }
+        else if(marks >= 30){
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Grade F");
+        }
+
+    }
+}
