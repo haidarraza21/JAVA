6a75e8adeb3e5dd04f4ae4fb44e6cd4c4167d624
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 18:46:50 +0530

    Update AreaTriangle.java

---
+++ AreaTriangle.java
@@ -0,0 +1,14 @@
+//Create a program to calculate the area of a Triangle. Area of triangle = 1/2*B*H
+import java.util.Scanner;
+public class AreaTriangle {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the base of the triangle :");
+        int base = input.nextInt();
+        System.out.print("Please enter the height of the triangle :");
+        int height = input.nextInt();
+
+        int area = (base * height)/2;
+        System.out.println("The area of a Triangle is :" + area);
+    }
+}
