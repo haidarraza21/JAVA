4993bf91c481126e875d67ff2a43240001c38795
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:14:22 +0530

    Update NumberIsPositiveNegativeOrZero.java

---
+++ NumberIsPositiveNegativeOrZero.java
@@ -0,0 +1,17 @@
+// Create a program that determines if a number is positive, negative pr zero.
+import java.util.Scanner;
+public class NumberIsPositiveNegativeOrZero {
+     public static void main(String[] args) {
+         Scanner input = new Scanner (System.in);
+         System.out.print("Please enter your number :");
+         int number = input.nextInt();
+
+         if(number > 0){
+             System.out.println("Number is Positive!");
+         } else if (number == 0) {
+             System.out.println("Number is Zero!");
+         } else {
+             System.out.println("Number is Negative");
+         }
+     }
+}
