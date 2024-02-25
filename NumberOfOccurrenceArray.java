df203f4bf99222ef5bdb8b2449015e52925792b8
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:15:30 +0530

    Update NumberOfOccurrenceArray.java

---
+++ NumberOfOccurrenceArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//Create a program to find number of occurrences of an element in an array.
+public class NumberOfOccurrenceArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Array occurrences");
+        Scanner input = new Scanner(System.in);
+        int [] numArr = ArrayUtility.inputArray();
+        System.out.print("Now enter the number you want to find: ");
+        int num = input.nextInt();
+
+        int occurrences = noOfOccurrences(numArr, num);
+        System.out.println("Your element was found " + occurrences +" times in the array");
+    }
+    public static int noOfOccurrences(int[] numArr, int num){
+        int occ =0;
+        int i = 0;
+        while (i < numArr.length){
+            if (numArr[i]==num){
+                occ++;
+            }
+            i++;
+        }
+        return occ;
+    }
+}
