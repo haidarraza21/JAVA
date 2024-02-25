9a2da6ddeba1b18c5b2730fd2a716dd57f0af103
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:00:32 +0530

    Update FactorialNumber.java

---
+++ FactorialNumber.java
@@ -0,0 +1,25 @@
+//Write a function that calculate the factorial of the given number.
+import java.util.Scanner;
+public class FactorialNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Please enter your number: ");
+        int num = input.nextInt();
+        int fact = factorial(num);
+        System.out.println(fact);
+
+    }
+    public static int factorial(int num){
+        if(num < 2){
+            return 1;
+        }
+        int fact = 1;
+        int i = 2;
+        while (i <= num){
+            fact*= i;
+            i ++;
+
+        }
+        return fact;
+    }
+}
