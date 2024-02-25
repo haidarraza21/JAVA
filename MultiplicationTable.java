7c6a1460b3fd0c782c38e35088d54263ecce01b9
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:13:27 +0530

    Update MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,19 @@
+//Develop a program that prints the multiplication table for a given number.
+import java.util.Scanner;
+public class MultiplicationTable {
+     public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+         System.out.print("Please enter your number: ");
+         int num = input.nextInt();
+         multiplicationNumber(num);
+
+
+     }
+     public static void multiplicationNumber(int num){
+         int i = 1 ;
+         while (i<=10){
+             System.out.println(num + " * " + i + " = " + i*num);
+             i ++;
+         }
+     }
+}
