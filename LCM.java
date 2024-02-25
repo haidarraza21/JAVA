690d05b4cfeec92f38ee1690629890e2ee916469
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:07:59 +0530

    Update LCM.java

---
+++ LCM.java
@@ -0,0 +1,26 @@
+//Create a program to find the least Common Multiple (LCM) of the numbers
+import java.util.Scanner;
+public class LCM {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your number: ");
+        int first = input.nextInt();
+        System.out.print("Please enter your second number: ");
+        int second = input.nextInt();
+        int lcm= lcm(first,second);
+        System.out.println("LCM of two number is: "+lcm);
+
+
+    }
+    public static int lcm (int first, int second){
+        int i =1;
+        while (true){
+            int factor =first * i;
+            if (factor% second == 0){
+                return factor;
+            }
+            i++;
+        }
+
+    }
+}
