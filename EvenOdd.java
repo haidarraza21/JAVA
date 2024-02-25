4732082e134ac72dfd177f61be418dd73ce425d1
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 18:58:33 +0530

    Update EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+//Create a program that determines if a number is odd or even.
+import java.util.Scanner;
+public class EvenOdd {
+     public static void main(String[] args) {
+         Scanner input = new Scanner (System.in);
+         System.out.print("Please enter your number :");
+         int number = input.nextInt();
+
+         if(number % 2 == 0){
+             System.out.println("Number is Even!");
+         } else {
+             System.out.println("Number is Odd!");
+         }
+     }
+}
