f26166c9a6940affabcfdd3e9e01160924cc844a
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 18:51:20 +0530

    Update BitwiseCompliment.java

---
+++ BitwiseCompliment.java
@@ -0,0 +1,12 @@
+//Create a program that shows bitwise compliment of a number.
+import java.util.Scanner;
+public class BitwiseCompliment {
+  public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   System.out.print("Please enter the first number :");
+   int a = input.nextInt();
+
+   int bitwiseCompliment= ~a ;
+   System.out.println("The bitwise Compliment is " + bitwiseCompliment);
+  }
+}
