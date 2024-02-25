241c4e3be766a3c0fe8f0de0a46228870182633f
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 19:12:48 +0530

    Update MergeTwoArray.java

---
+++ MergeTwoArray.java
@@ -0,0 +1,33 @@
+//Create a program to merge two sorted arrays.
+public class MergeTwoArray {
+    public static void main(String[] args) {
+        int[] arr1 = ArrayUtility.inputArray();
+        int[] arr2 = ArrayUtility.inputArray();
+        int[] mergeArr = merge(arr1, arr2);
+        System.out.println("Your merges array is: ");
+        ArrayUtility.displayArray(mergeArr);
+    }
+
+    public static int[] merge(int[] arr1, int[] arr2){
+        int newSize = arr1.length + arr2.length;
+        int[] newArr = new int[newSize];
+        int i = 0; int j = 0; int k = 0;
+        while (i < arr1.length || j< arr2.length){
+            if (j == arr2.length ||
+                    (i < arr1.length && arr1[i] < arr2[j])){
+                newArr[k] = arr1[i];
+                i++;
+                k++;
+
+            }
+            else {
+                newArr[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+
+    return  newArr;
+    }
+
+}
