ea2a0cd4827e156eaa5734d1bd2bc9a9f8214cc2
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-02-25 18:50:42 +0530

    Update BitwiseAND.java

---
+++ BitwiseAND.java
@@ -0,0 +1,16 @@
+//Create a program that shows bitwise AND of two numbers
+import java.util.Scanner;
+public class BitwiseAND {
+ public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+  System.out.print("Please enter the first number :");
+  int num1 = input.nextInt();
+  System.out.print("Please enter the second number :");
+  int num2 = input.nextInt();
+
+  int result = num1 & num2;
+
+  System.out.println("The bitwise AND of these numbers is :" + result);
+
+ }
+}
