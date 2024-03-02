5c16c46626a5aa60f8482a766c1e552b3ad4fd10
Md Haidar Raza <141956922+haidarraza21@users.noreply.github.com>
2024-03-02 23:40:20 +0530

    Update AgeGroups.java

--- AgeGroups.java
+++ AgeGroups.java
@@ -1,5 +1,5 @@
 /*
-Create a program that categorize a person into different age groups
+Create java a program that categorize a person into different age groups
 Child -> below 13           Teen->below 20
 adult-> below 60            Senior->above 60*/
 
